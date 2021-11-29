473117e8b28e8fb3dad813cc647129baac202698
Kirill <budanovkirilldmitrievich@gmail.com>
2021-11-29 15:28:27 +0300

    dz

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -106,6 +106,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'NAME': 'geekshop',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'USER': 'postgres',
+    # }
 }
 
 
