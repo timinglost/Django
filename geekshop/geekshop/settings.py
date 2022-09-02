a569c85553c946431bf8a898855bedf175245f57
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-02 21:16:34 +0300

    web

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -159,11 +159,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "geekshop", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "geekshop", "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "geekshop", "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
