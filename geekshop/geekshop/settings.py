2e345c4912a2f50dd0df59ad4a81b107079e3673
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-02 21:37:36 +0300

    web

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -62,6 +62,7 @@ AUTHENTICATION_BACKENDS = (
 MIDDLEWARE = [
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,7 +160,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "geekshop", "static"),
