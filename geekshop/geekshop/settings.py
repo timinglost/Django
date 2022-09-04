b94bc3f3c50dfe23bdecbf8b4558a3bf871b05c1
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-04 17:03:45 +0300

    web

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -162,9 +162,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "geekshop", "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "geekshop", "static"),
+# )
 
 
 # Default primary key field type
