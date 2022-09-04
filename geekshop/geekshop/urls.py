c1d844428b5146e656d441962f9647be948ba3be
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-04 17:10:23 +0300

    web

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='orders')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
 if settings.DEBUG:
     import debug_toolbar
 
