7cdd25c675c5be430b4e47bd61bb4b118e9b21b9
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-05 17:52:16 +0300

    web

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from .views import main, contacts
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,11 +32,15 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='orders')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.MEDIA_ROOT,
+                            }),
+
 
 
 
