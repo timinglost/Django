80f3e0a2bb5febe4c39f727e5df596b7e19f98ea
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-05 17:57:05 +0300

    web

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -32,15 +32,11 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='orders')),
 
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-                                'document_root': settings.MEDIA_ROOT,
-                            }),
-
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
