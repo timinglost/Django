d7dd1df1c55e3d188c5629c60c8cc3d6e876e770
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-05 19:40:54 +0300

    web

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'adminapp',
     'ordersapp',
     'social_django',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'template_profiler_panel',
     'django_extensions',
 ]
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
@@ -88,7 +88,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
+                # 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -31,12 +31,12 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='orders')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    # urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
